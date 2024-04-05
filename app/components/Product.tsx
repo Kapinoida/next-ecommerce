9567a820402843f86f2b9316f677267bdac8e63e
David Plaskett <dcplaskett@gmail.com>
2024-04-04 20:17:27 -0500

    Updated Product Descriptions and created Product Pages

--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -1,15 +1,19 @@
 import Image from 'next/image'
 import formatPrice from '@/util/PriceFormat'
 import { ProductType } from '@/types/ProductType'
+import Link from 'next/link'
 
-export default function Product({ name, image, price }: ProductType) {
+export default function Product({ name, image, unit_amount, id, description, metadata }: ProductType) {
+    const { features } = metadata
     return (
-        <div className='text-gray-700'>
-            <Image className='w-full h-96 object-cover rounded-lg' src={image} width={800} height={800} alt={name} />
-            <div className='font-medium py-2'>
-                <h1>{name}</h1>
-                <h2 className='text-sm'>{price !== null ? formatPrice(price as number) : 'N/A'}</h2>
+        <Link href={{pathname: `/products/${id}`, query: {name, image, unit_amount, id, description, features}}}>
+            <div className='text-gray-700'>
+                <Image className='w-full h-96 object-cover rounded-lg' src={image} width={800} height={800} alt={name} />
+                <div className='font-medium py-2'>
+                    <h1>{name}</h1>
+                    <h2 className='text-sm'>{unit_amount !== null ? formatPrice(unit_amount as number) : 'N/A'}</h2>
+                </div>
             </div>
-        </div>
+        </Link>
     )
 }
